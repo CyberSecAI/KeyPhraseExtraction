e0bc2d7a1ef9ddc7646561e53b04a727a3754127
crashedmind <crashedmind@gmail.com>
2025-07-05 09:39:21 +0100

    update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+python3 ./keyphraseExtract.py 
 python3 keyphraseExtract_check.py
-python3 json_fix.py
-python3 json_fix.py --copy-back
+
+# now using structured json so these are not required:
+#python3 json_fix.py
+#python3 json_fix.py --copy-back
+
 python3 merge_jsons2all.py
 python3 move2cve_dir_hash.py
